1b57c40d4b9b56f82a1fb8c86edfb324674959d5
Kunal Shah <95049390+kunalshah017@users.noreply.github.com>
2023-05-03 06:22:07 +0000

    minor fixes

--- main.cpp
+++ main.cpp
@@ -757,14 +757,14 @@ int main()
         cout << "\n-----------------------" << endl;
         cout << "\nChoose option by pressing option number eg.1,2,3.." << endl;
 
-        cout << "\n1.Open new a Savings Account" << endl;
-        cout << "2.Open a Existing Savings Account" << endl;
-        cout << "3.Forgot Pin of your Savings Account" << endl;
-        cout << "4.Change Pin of your Savings Account" << endl;
-        cout << "5.Show Balance in Savings Account" << endl;
-        cout << "6.Add Money to Savings Account" << endl;
-        cout << "7.Withdraw Money form Savings Account" << endl;
-        cout << "8.Logout & Exit" << endl;
+        cout << "\n1. Open new a Savings Account" << endl;
+        cout << "2. Open a Existing Savings Account" << endl;
+        cout << "3. Forgot Pin of your Savings Account" << endl;
+        cout << "4. Change Pin of your Savings Account" << endl;
+        cout << "5. Show Balance in Savings Account" << endl;
+        cout << "6. Add Money to Savings Account" << endl;
+        cout << "7. Withdraw Money form Savings Account" << endl;
+        cout << "8. Logout & Exit" << endl;
         cout << "\n-----------------------" << endl;
 
         cout << "\n>> ";
